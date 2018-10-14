4e22385550a23bfb8d1024f5e54790a9a9a42dbc
Kelu <kelu124@gmail.com>
2018-10-14 15:57:10 +0200

    20181014

---
+++ pHAT/20181014/code/ad.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pigpio.h>
+
+/*
+   gcc -pthread -o spi-speed spi-speed.c -lpigpio
+*/
+
+int main(int argc, char *argv[])
+{
+   int i;
+   int h;
+   int loops;
+   int speed;
+   double start, diff, sps;
+   unsigned char buf[2];
+   unsigned char resp[2];
+   if (argc > 1) loops = atoi(argv[1]);
+   else loops = 1000000;
+
+   if (argc > 2) speed = atoi(argv[2]);
+   else speed = 1000000;
+
+   if (gpioInitialise() < 0) return 1;
+
+   h = spiOpen(0, speed, 0);
+
+   if (h < 0) return 2;
+
+   start = time_time();
+
+   for (i=0; i<loops; i++)
+   {
+      buf[0] = 104; // x start_bit single_mode ch1 msbf xxx
+      buf[1] = 0;
+
+      spiXfer(h, buf, resp, 2);
+   
+      printf("%d %d\n", resp[0],resp[1]);
+   }
+
+   diff = time_time() - start;
+   printf("%f\n",diff);
+   printf("sps=%.1f @ %d bps (%d/%.1f)\n",
+      (double)loops / diff, speed, loops, diff);
+
+   spiClose(h);
+
+   gpioTerminate();
+
+   return 0;
+}
---
+++ pHAT/20181014/code/results.md
@@ -0,0 +1,21 @@
+
+# Without
+sps=82451.3 @ 1000000 bps (1000000/12.1)
+
+# 
+pi@ricklite:~/mike/c $ sudo ./spi-speed  1000000 4000000
+sps=275283.9 @ 4000000 bps (1000000/3.6)
+
+pi@ricklite:~/mike/c $ sudo ./spi-speed  1000000 26000000
+sps=783389.8 @ 26000000 bps (1000000/1.3)
+
+pi@ricklite:~/mike/c $ sudo ./spi-speed  1000000 64000000
+sps=939186.0 @ 64000000 bps (1000000/1.1)
+
+
+
+pi@ricklite:~/mike/c $ sudo ./spi-speed  1000000 34000000
+sps=855990.7 @ 34000000 bps (1000000/1.2)
+
+:wq
+
